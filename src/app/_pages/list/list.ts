ce2b2aa5705ed8c4de08fb2e79b9b0be4f53ec27
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-06-12 00:19:13 +0330

    ADD: List

--- src/app/_pages/list/list.ts
+++ src/app/_pages/list/list.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {Note} from '../../_models/note.model';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './list.html',
+  standalone: true,
   styleUrl: './list.css'
 })
 export class List {
-
+  notes: Note[] = [
+    new Note(1, 'test Title', 'Desc Test'),
+    new Note(2, 'test Title 2', 'Desc Test 2'),
+    new Note(3, 'test Title 3', 'Desc Test 3'),
+    new Note(4, 'test Title 4', 'Desc Test 4')
+  ]
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'mobile-class-university';
+
 }
